453bf5d34c9784780883d95b56d3fdc7a45e7682
Ragul P <66929382+ragulpalanisamy@users.noreply.github.com>
2023-04-03 14:49:41 +0530

    initial commit

--- user-details.js
+++ user-details.js
@@ -1,7 +1,6 @@
- const searchButton = document.getElementById("btn");
+const searchButton = document.getElementById("btn");
 const searchInput = document.getElementById("input");
 const userCardContainer = document.getElementById("user-card");
-const userDetailContainer = document.getElementById("user-detail-container");
 const clear = document.getElementById("clear");
 const reset = document.getElementById("reset");
 
@@ -9,7 +8,7 @@ const reset = document.getElementById("reset");
 clear.addEventListener("click", () => {
     searchInput.value = "";
     userCardContainer.innerHTML = "";
-    userDetailContainer.innerHTML = "";
+    localStorage.clear();
 });
 
 // Event listener for reset button
@@ -19,21 +18,40 @@ reset.addEventListener("click", () => {
 
 // Event listener for search button
 searchButton.addEventListener("click", async () => {
-    try {
-        const response = await fetch(`https://api.github.com/users/${searchInput.value}`);
-        if (!response.ok) {
-            throw new Error("User not found!");
-        }
-        const userData = await response.json();
-        renderUserCard(userData);
-    } 
-    catch (error) {
-        alert(error);
-    }
+  try {
+      const response = await fetch(`https://api.github.com/users/${searchInput.value}`);
+      if (!response.ok) {
+          throw new Error("User not found!");
+      }
+      const userData = await response.json();
+      renderUserCard(userData);
+      localStorage.setItem("githubUsername", searchInput.value); // save username to local storage
+  } 
+  catch (error) {
+      alert(error);
+  }
+});
+
+window.addEventListener("load", async () => {
+  const githubUsername = localStorage.getItem("githubUsername");
+  if (githubUsername) {
+      try {
+          const response = await fetch(`https://api.github.com/users/${githubUsername}`);
+          if (!response.ok) {
+              throw new Error("User not found!");
+          }
+          const userData = await response.json();
+          renderUserCard(userData);
+      } 
+      catch (error) {
+          alert(error);
+      }
+  }
 });
 
 // Function to render user card
 function renderUserCard(userData) {
+  // create the user card element
   const card = document.createElement("div");
   card.classList.add(
     "user-card",
@@ -43,6 +61,7 @@ function renderUserCard(userData) {
     "p-4"
   );
 
+  // create the user avatar element
   const avatar = document.createElement("img");
   avatar.classList.add(
     "avatar",
@@ -59,18 +78,27 @@ function renderUserCard(userData) {
     window.location.href = userData.html_url;
   });
 
-  const username = document.createElement("a");
-  username.href = userData.html_url;
-  username.target = "_blank";
-
+  // create the user name element
   const name = document.createElement("h2");
   name.classList.add("name", "text-xl", "font-bold", "text-gray-800");
   name.textContent = userData.name;
 
-  const bio = document.createElement("h2");
-  bio.classList.add("bio", "text-md", "text-gray-800", "mt-2");
-  bio.textContent = userData.bio;
+  // create the user location element
+  const location = document.createElement("p");
+  location.classList.add("location", "text-md", "text-gray-800", "mt-2");
+  location.textContent = userData.location;
 
+  // create the user company element
+  const company = document.createElement("p");
+  company.classList.add("company", "text-md", "text-gray-800", "mt-2");
+  company.textContent = userData.company;
+
+  // create the user bio element
+  const bio = document.createElement("p");
+  bio.classList.add("bio", "text-md", "text-gray-800", "mt-2");
+  bio.textContent = `Bio: ${userData.bio}`;
+
+  // create the user followers element
   const followers = document.createElement("a");
   followers.classList.add(
     "followers",
@@ -83,6 +111,7 @@ function renderUserCard(userData) {
   followers.href = `${userData.html_url}?tab=followers`;
   followers.textContent =`Followers`;
 
+  // create the user following element
   const following = document.createElement("a");
   following.classList.add(
     "following",
@@ -96,36 +125,63 @@ function renderUserCard(userData) {
   following.textContent =`Following`;
   following.style.marginLeft = "10px";
 
-  const repo = document.createElement("a");
-  repo.classList.add(
-    "repo",
+  // create the user repos element
+  const repos = document.createElement("a");
+  repos.classList.add(
+    "repos",
     "block",
     "text-lg",
     "font-bold",
     "text-gray-800",
     "mt-2"
   );
-  repo.href = `${userData.html_url}?tab=repositories`;
-  repo.textContent = `Repositories`;
-  repo.style.marginLeft = "10px";
+  repos.href = `${userData.html_url}?tab=repositories`;
+  repos.textContent = `Repositories`;
+  repos.style.marginLeft = "10px";
 
+  // create the user orgs element
+  const orgs = document.createElement("a");
+  orgs.classList.add(
+    "orgs",
+    "block",
+    "text-lg",
+    "font-bold",
+    "text-gray-800",
+    "mt-2"
+  );
+  orgs.href = `${userData.html_url}?tab=organizations`;
+  orgs.textContent = `Organizations`;
+  orgs.style.marginLeft = "10px";
+
+  // append all the elements to the card
   card.appendChild(avatar);
-  card.appendChild(username);
   card.appendChild(name);
+  card.appendChild(location);
+  card.appendChild(company);
   card.appendChild(bio);
   card.appendChild(followers);
   card.appendChild(following);
-  card.appendChild(repo);
+  card.appendChild(repos);
+  card.appendChild(orgs);
 
+  // append the card to the user card container
   userCardContainer.appendChild(card);
-
-  // Add click event listener to each repo to redirect to its GitHub page
-  const repoLinks = card.querySelectorAll(".repo-name");
-  repoLinks.forEach((link) => {
-    link.addEventListener("click", (event) => {
-      event.preventDefault();
-      window.open(link.href, "_blank");
-    });
-  });
 }
+// Store user data in local storage
+localStorage.setItem("userData", JSON.stringify(userData));
 
+// Add click event listener to each repo to redirect to its GitHub page
+const repoLinks = card.querySelectorAll(".repo-name");
+repoLinks.forEach((link) => {
+link.addEventListener("click", (event) => {
+event.preventDefault();
+window.open(link.href, "_blank");
+});
+});
+
+// Get user data from localStorage
+const userDataJSON = localStorage.getItem("userData");
+if (userDataJSON) {
+  const userData = JSON.parse(userDataJSON);
+  renderUserCard(userData);
+}
