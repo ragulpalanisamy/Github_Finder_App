6eeab951b7f2a0ff3013264af31d0891b946e3e7
ragulpalanisamy <ragulpalanisamy1001@gmail.com>
2023-12-17 17:06:11 +0530

    feat: :sparkles: Added the slack message integration

--- github-UserFinder/src/App.tsx
+++ github-UserFinder/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import GithubUserFinder from "./component";
+import GithubUserFinder from "./components";
+import Slack from "./components/Integration/Slack";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GithubUserFinder />} />
+        <Route path="/slack" element={<Slack />} />
       </Routes>
     </BrowserRouter>
   );
